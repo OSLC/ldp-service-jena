b1350a94b674322d7c052981779c0efe49b0df62
Jim Amsden <jamsden@us.ibm.com>
2018-04-26 08:54:28 -0400

    Updated dependencies and factored to be an LDP storage service
    Signed-off-by: Jim Amsden <jamsden@us.ibm.com>

--- README.md
+++ README.md
@@ -1,11 +1,10 @@
 # ldp-service-jena
 
-A simple Node.js module providing Express middleware to create a [W3C Linked Data Platform](http://www.w3.org/2012/ldp) server. The service uses Apache Jena Fuseki for persistence, jsonld.js for JSON-LD support, and a few other JavaScript libraries.  A sample app using the LDP middleware service is running at [http://ldp-app.mybluemix.net](http://ldp-app.mybluemix.net).
+Provides a concrete implementation of the ldp-service/storage.js abstract storage services module.
 
 ldp-service-jena supports LDP basic and direct containers. Indirect
 containers and non-RDF source are not implemented.
 
-Many thanks to Steve Speicher and Sam Padgett for their valuable contribution to LDP and this LDP middleware.
 
 Module planning, maintenance and issues can be see at at the [ldp-service](https://hub.jazz.net/project/jamsden/ldp-service/overview) IBM Bluemix DevOps Services project.
 
@@ -16,14 +15,16 @@ Module planning, maintenance and issues can be see at at the [ldp-service](https
 
 Install [Node.js](http://nodejs.org). 
 
-Start [Jena](https://jena.apache.org/download/index.cgi). Download apache-jena-fuseki-2.4.1.tar.gz under Apache Jena Fuseki and unzip it.
+2) Start [Jena](https://jena.apache.org/download/index.cgi). 
 
-To run Jena, enter the following code
+Download apache-jena-fuseki-2.4.1.tar.gz under Apache Jena Fuseki and unzip it.
+
+To run Jena, enter the following code, providing the path to the datastore:
 
 	$ fuseki-server --mem /ldp
 
 /ldp is a datastore that allows the request to access the resources on the db. It can be named in any other way. --mem allows for temporary storage of data
-for that instant. For the data to permantently store data (and to update data), the following code should be ran.
+for that instant. For the data to permanently stored, and to update data, the following code should be ran.
 
 	$ fuseki-server --update --loc=<path to db> /ldp
 
@@ -34,13 +35,14 @@ Install express.js and create a sample express app
 	$ npm install express -g
 	$ express --git -e <appDir>
 
-2) Edit app.js and add whatever Express middleware you need including ldp-service. ldp-service-jena also provides access to its Apache Jena database in case additional middleware needs direct access to the database. ldp-service-jena has not been published to npm yet, so it will need to be access locally.
+3) Edit app.js and add whatever Express middleware you need including ldp-service. ldp-service-jena also provides access to its Apache Jena database in case additional middleware needs direct access to the database.
 
-	var ldpService = require('./ldp-service-jena');
-	app.use(ldpService());
-	var db = ldpService.db; // incase further middleware needs access to the database
+	var ldpService = require('ldp-service');
+	var env = require('./env.js');
+	app.use(ldpService(env))
+	var db = ldpService.db // incase further middleware needs access to the database
 
-3) Configuration defaults can be found in config.json. These may be overridden by variables in the environment, including Bluemix variables if deployed in a Bluemix app.
+4) Configuration should be speciried in env.js. These may be overridden by variables in the environment, including Bluemix variables if deployed in a Bluemix app.
 
 4) To start the app, run these commands
 
@@ -51,10 +53,7 @@ Finally, point your browser to
 [http://localhost:3000/](http://localhost:3000/).
 
 
-## Differences From 'ldp-service'
-
-ldp-service-jena has not been configured to use the visualization tool that was included in ldp-service. Any testing will need to be done through a
-third-party program.
+## Additional Notes
 
 The file db.js uses the 'request' package from npm to perform HTTP requests on Apache Jena Fuseki. It does not directly connect with the database. Rather, it
 finds the location of the Apache Jena db instance using the URI in the configuration.
@@ -63,30 +62,24 @@ The code does not have its own resources to upload if there is no root in the da
 file. This is a recursive operation at loads all resources with new URI's. It assumes that the default services are written in JSON-LD.
 
 
-## Example of a Configuration File
+## Example of a env.js File
 
-ldp-service-jena assumes that the configuration file is written with a certain set of properties. Here is an example.
+ldp-service-jena assumes that the environment configuration file provided by applications is written with a certain set of properties. Here is an example.
 
-{
-	"scheme": "http",
-	"host": "localhost",
-	"port": 3000,
-	"context": "/r",
-	"JenaURL": "http://localhost:3030/ldp/",
-	"services": *path to* "./config/defaultServices.json"
-}
+    {
+		"scheme": "http",
+		"host": "localhost",
+		"port": 3000,
+		"context": "/r",
+		"JenaURL": "http://localhost:3030/ldp/",
+    }
 
 
 ## License
 
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
+Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License. You may obtain a copy of the License at
 
    http://www.apache.org/licenses/LICENSE-2.0
 
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
+Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and limitations under the License.
---
+++ storage.js
@@ -0,0 +1,300 @@
+/*
+ * Copyright 2014 IBM Corporation.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+
+/*
+ * storage.js implements the abstract ldp-service/storage.js module by
+ * storing RDF graphs in Apache Jena. Each document representations
+ * an RDF graph. Documents have the properties below. The 'triples'
+ * property is the RDF. Other properties are metadata.
+ *
+ * This implementation delegates all the database requests to Fuesiki 
+ * using the JenaURL from the env parameter
+ *
+ * All documents:
+ *
+ *   name - the URI of the graph
+ *   interactionModel - the URI indicating the LDP interaction model of the resource
+ *   container - the container for this resource
+ *   deleted - boolean indicating if the resource has been deleted (to avoid reusing URIs)
+ *   triples - an array of RDF triples in N3.js format
+ *
+ * Direct containers:
+ *
+ *   membershipResource - the ldp:membershipResource property
+ *   hasMemberRelation - the ldp:hasMemberRelation property
+ *   isMemberOfRelation - the ldp:isMemberOfRelation property
+ *
+ * Membership resources (resources in a container):
+ *
+ *   membershipResourceFor - the associated direct container (always 1:1)
+ *
+ * Rather than storing a link to all of its members in the container,
+ * we have a property in each resource that points back to its
+ * container. On a container GET, we query for a container's resources
+ * and mix in containment triples.
+ */
+
+module.exports = (function(storage_services) {
+
+var ldp = require('./vocab/ldp.js') // LDP vocabulary
+var request = require('request')
+var db
+
+storage_services.init = function(env, callback) {
+	db = env.JenaURL
+	callback()
+}
+
+storage_services.reserveURI = function(uri, callback) {
+	// simply create a document with only a URI. we will 
+	// just update it later on put
+	// if it fails, we reject the uri
+	var options = {
+        uri: db+"data?graph="+uri.toLowerCase(),
+        method: "PUT",
+        headers: {
+        	'Content-Type': 'application/ld+json'
+        },
+        body: null
+    }   
+    request(options, function(err, ires, body) {
+    	callback(err, ires);
+    })
+}
+
+storage_services.releaseURI = function(uri) {
+	var options = {
+        uri: db+"data?graph="+uri.toLowerCase(),
+        method: "DELETE",
+    }
+    request(options, function(err, ires, body){
+        if (err) ires.send(500)
+    })
+}
+
+storage_services.get = function(uri, callback) {
+    var options = {
+        uri: db+"data?graph="+uri.toLowerCase(),
+        method: "GET",
+        headers: {
+            "Accept": content_type
+        }          
+    }
+    request(options, function(err, ires, body) {
+        console.log("REQUEST " + options.uri)
+        callback(err, ires);
+    })
+}
+
+storage_services.put = function(doc, callback) {
+	var options = {
+        uri: db+"data?graph="+uri.toLowerCase(),
+        method: "PUT",
+        headers: {
+         	'Content-Type': content_type
+        },
+        body: doc
+    }
+    request(options, function(err, ires, body) {
+    	callback(err, ires);
+    })
+}
+
+storage_services.remove = function(uri, callback) {
+	var options = {
+        uri: db+"update?graph="+uri.toLowerCase(),
+        method: "DELETE",
+    }
+    request(options, function(err, ires, body) {
+        callback(err, ires);
+    })
+}
+
+storage_services.findContainer = function(uri, callback) {
+	throw "storage method fincContainer(uri, callback) not implemented"
+}
+
+storage_services.getContainment = function(uri, callback) {
+	throw "storage method getContainment(uri, callback) not implemented"
+}
+
+storage_services.createMembershipResource = function(document, callback) {
+	throw "storage method createMembershipResource(document, callback) not implemented"
+}
+
+storage_services.drop = function(callback) {
+    throw "storage method drop(callback) not implemented"
+}
+
+
+exports.query = function(ast, base, callback) {
+	// convert to SPARQL and execute
+    var node = ast
+    var stack = new Array()
+    var var_stack = new Array()
+
+    if (node.right.val === "*") { 
+        var uri = "SELECT ?g WHERE {GRAPH ?g { ?s ?p ?o } }"
+        var options = {
+            uri: db+"query?query="+uri,
+            method: "GET",
+            headers: {
+                "Accept": "application/sparql-results+json"
+            }               
+        }
+
+        request(options, function(err, ires, body) {
+            console.log("REQUEST " + options.uri)
+            callback(err, ires);
+        })
+    } else {
+        var sparql_query_select = "SELECT ?g ";
+        var sparql_query_where = "WHERE { GRAPH ?g { ";
+        var sparql_query_prefix = "";
+        var sparql_query_orderBy = "";
+
+        var found =  {
+            "oslc.select": false,
+            "oslc.where": false,
+            "oslc.orderBy": false,
+            "oslc.prefix": false
+        }
+
+        while (node.left != null && node.right != null) {
+            while(node.left != null){
+                stack.push(node)
+                node = node.left
+            }
+
+            if (node.val === "oslc.select") {
+                found["oslc.where"] = false
+                found["oslc.orderBy"] = false
+                found["oslc.prefix"] = false
+                found["oslc.select"] = true
+                var_stack.push("?s")
+                node = node.pop().right
+            } else if(node.val === "oslc.where") {
+                found["oslc.select"] = false
+                found["oslc.orderBy"] = false
+                found["oslc.prefix"] = false
+                found["oslc.where"] = true
+                node = stack.pop().right
+            } else if(node.val === "oslc.prefix") {
+                found["oslc.select"] = false
+                found["oslc.orderBy"] = false
+                found["oslc.where"] = false
+                found["oslc.prefix"] = true
+                node = stack.pop().right
+            } else if(node.val === "oslc.orderBy") {
+                found["oslc.select"] = false
+                found["oslc.where"] = false
+                found["oslc.prefix"] = false
+                found["oslc.orderBy"] = true
+                node = stack.pop().right
+            } else if(node.val === "oslc.searchTerms") {
+                found["oslc.searchTerms"] = true
+            } else {
+                if (found["oslc.prefix"]){
+                    sparql_query_prefix+="PREFIX "+node.val+": "
+                    node = stack.pop().right
+                    sparql_query_prefix+=node.val+" "
+                    node = stack.pop().right
+                }
+                if (found["oslc.orderBy"]) {
+                    if (node.val.charAt(0) === '-') {
+                        sparql_query_orderBy += "DESC(?"+node.val.substring(1, node.val.length).replace(':','_') + ") "
+                    } else if(node.val.charAt(0) === '+') {
+                        sparql_query_orderBy += "ASC(?"+node.val.substring(1, node.val.length).replace(':','_') + ") "
+                    } else {
+                        sparql_orderBy += "?"+query.substring(index, i).replace(':','_')+" "
+                    }
+                    node = stack.pop().right
+                }
+
+                if (found["oslc.select"]) {
+                    sparql_query_select += "?"+node.val.replace(':','_')+" "
+                    sparql_query_where += var_stack[stack.length-1]+ " " + node.val + " ?" + node.val.replace(':', '_') + " . "
+                    var new_var = node.val.replace(':','_')
+                    node = stack.pop()
+
+                    /*
+                        if (node.val === '{') {
+                            var_stack.push(new_var)
+                        }
+
+                        if (node.val === '}') {
+                            if (var_stack[var_stack.length-1] !== "?s") {
+                                var_stack.pop()    
+                            } else {
+                                callback("Syntax Error")
+                            }
+                        }
+                    */
+
+                    node = node.right                    
+                }
+
+                if (found["oslc.where"]) {
+                    sparql_query_where += "?s " + node.val + " "
+                    node = stack.pop()
+                    sparql_query_where += node.right.val + " . "
+
+                    // node = stack.pop().right
+                    node = node.right
+
+                    // DOES NOT consider filters
+                }
+
+            }
+
+        }
+
+        uri = sparql_query_prefix+sparql_query_select+sparql_query_where+sparql_query_orderBy + "} }"
+
+        var options = {
+            uri: db+"query?query="+uri,
+            method: "GET",
+            headers: {
+                "Accept": "application/sparql-results+json"
+            }              
+        }
+
+        request(options, function(err, ires, body) {
+            console.log("REQUEST " + options.uri);
+            if (err) {
+                callback(err)
+                return
+            }
+
+            var triples = []
+            body = JSON.parse(body)
+            if (body["results"]["bindings"].length === 0) {
+                callback(err, ires);
+            } else {
+                for (var i = 0; i < JSON.parse(body)["results"]["bindings"].length; i++) {
+                    triples.push({"subject": base, "predicate": rdf.resource, "object": body["results"]["bindings"][i]["uri"]})
+                }
+
+                jsonld.serialize(triples, function(err, result){
+                    ires.body = result
+                    callback(err, ires)
+                })
+            }                       
+        })
+    }
+}
+})
